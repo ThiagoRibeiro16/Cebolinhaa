8aa58bd224c4daa74aff51bbaa8054d6eda38dc6
ThiagoRibeiro16 <thiago.escola321@gmail.com>
2025-03-22 14:25:23 -0300

    finaliza programa

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+string fala;
+
+string falaErrada;
+
+Console.Write("Digite uma frase: ");
+fala = Console.ReadLine()!.ToLower();
+
+falaErrada = fala.Replace("r", "l");
+
+Console.WriteLine($"\nfrase original: {fala}");
+
+Console.WriteLine($"\nfrase do cebolinha: {falaErrada}");
